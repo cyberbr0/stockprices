b7e8e72921f6cc55eec1d84a27e6a3983ebab19a
cyberbr0 <127756294+cyberbr0@users.noreply.github.com>
2023-07-12 15:09:32 -0400

    Update scrape_stock_prices.py

--- scrape_stock_prices.py
+++ scrape_stock_prices.py
@@ -1,11 +1,9 @@
 import yfinance as yf
 from datetime import datetime
 
-tickers = ['AAPL', 'BRK-A', 'META', 'MSFT', 'NVDA', 'TSLA'] 
+tickers = ['AAPL','BRK-A','META','MSFT','NVDA','TSLA'] 
 interval_minutes = 15
 now = datetime.now()
-
-
 
 stock_data = {}
 
@@ -15,13 +13,10 @@ for ticker in tickers:
     data = yf.download(ticker, start='2023-07-10', end=now.strftime('%Y-%m-%d'))
     stock_data[ticker] = data    
 
-
-    print(f'\n\nApple Inc | AAPL:',stock_data['AAPL'],'\n\n\n')
-    print('Berkshire Hathaway Inc Class A |BRK-A:',stock_data['BRK-A'],'\n\n\n')
-    #print(f'Meta Platforms Inc | META:',stock_data['META'],'\n\n\n')
-    #print(f'Microsoft Corp | MSFT:',stock_data['MSFT'],'\n\n\n')
-    #print(f'Nvidia Corp | NVDA:',stock_data['NVDA'],'\n\n\n')
-    #print(f'Tesla Inc | TSLA:', stock_data['TSLA'],'\n\n\n')
-
-
-    
+print('Stock Prices for APL, BRK-A, META, MSFT, NVDA, and TSLA\n\n')
+print('Apple Inc | AAPL:',stock_data['AAPL'],'\n\n')
+print('Berkshire Hathaway Inc Class A | BRK-A:',stock_data['BRK-A'],'\n\n')
+print('Meta Platforms Inc | META:',stock_data['META'],'\n\n')
+print('Microsoft Corp | MSFT:',stock_data['MSFT'],'\n\n')
+print('Nvidia Corp | NVDA:',stock_data['NVDA'],'\n\n')
+print('Tesla Inc | TSLA:', stock_data['TSLA'],'\n\n')

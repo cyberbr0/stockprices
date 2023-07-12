af6f50565fd7706e523334f4d1250ff66f8ebedd
cyberbr0 <127756294+cyberbr0@users.noreply.github.com>
2023-07-12 13:10:40 -0400

    Update -- Removed While Loop

--- scrape_stock_prices.py
+++ scrape_stock_prices.py
@@ -6,21 +6,22 @@ interval_minutes = 15
 now = datetime.now()
 
 
-while True:
-    stock_data = {}
-    
-    for ticker in tickers:
-        stock = yf.Ticker(ticker)
-        data = yf.download(ticker, start='2023-07-10', end=now.strftime('%Y-%m-%d'))
-        stock_data[ticker] = data    
-        
-    for ticker, data in stock_data.items():
-        print(f'\n\nApple Inc | AAPL:',stock_data['AAPL'],'\n\n\n')
-        print(f'Berkshire Hathaway Inc Class A |BRK-A:',stock_data['BRK-A'],'\n\n\n')
-        print(f'Meta Platforms Inc | META:',stock_data['META'],'\n\n\n')
-        print(f'Microsoft Corp | MSFT:',stock_data['MSFT'],'\n\n\n')
-        print(f'Nvidia Corp | NVDA:',stock_data['NVDA'],'\n\n\n')
-        print(f'Tesla Inc | TSLA:', stock_data['TSLA'],'\n\n\n')
 
-        #time.sleep(interval_minutes * 15)  #Can use this syntax to control sleep intervals
+stock_data = {}
+
+for ticker in tickers:
+    
+    stock = yf.Ticker(ticker)
+    data = yf.download(ticker, start='2023-07-10', end=now.strftime('%Y-%m-%d'))
+    stock_data[ticker] = data    
+
+
+    print(f'\n\nApple Inc | AAPL:',stock_data['AAPL'],'\n\n\n')
+    print(f'Berkshire Hathaway Inc Class A |BRK-A:',stock_data['BRK-A'],'\n\n\n')
+    print(f'Meta Platforms Inc | META:',stock_data['META'],'\n\n\n')
+    print(f'Microsoft Corp | MSFT:',stock_data['MSFT'],'\n\n\n')
+    print(f'Nvidia Corp | NVDA:',stock_data['NVDA'],'\n\n\n')
+    print(f'Tesla Inc | TSLA:', stock_data['TSLA'],'\n\n\n')
+
+
     
